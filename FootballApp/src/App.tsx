2b2384fb6883e37214f7a3c534896065c166d2ae
bmalvo <boydmalvo@gmail.com>
2024-08-22 12:00:32 +0200

    create queryClient

--- FootballApp/src/App.tsx
+++ FootballApp/src/App.tsx
@@ -1,6 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient();
+
 export const App = () => {
 
-  return <>
-    
-  </>
+  return <QueryClientProvider client={queryClient}>
+    <h1>testing header</h1>
+  </QueryClientProvider>
 }
