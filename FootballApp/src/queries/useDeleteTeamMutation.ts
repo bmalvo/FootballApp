a8dc8a909d74f453bead79ba4db5dc0746f9ea6f
bmalvo <boydmalvo@gmail.com>
2024-12-24 14:56:12 +0100

    create queries: useDeleteTeamMutation / useGetTeamListQuery

---
+++ FootballApp/src/queries/useDeleteTeamMutation.ts
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useApi } from "../hooks/useApi"
+import { TeamType } from "../types";
+
+
+export const useDeleteTeamMutation = (teamId: string) => {
+    
+    const { apiDelete } = useApi();
+    const queryClient = useQueryClient();
+    
+    const {  mutate, isPending } = useMutation({
+
+        mutationKey: ['teams', 'delete', teamId],
+        mutationFn: async (teamId: string) => {
+
+            return apiDelete<TeamType>(`teams/${teamId}`)
+        },
+        onSuccess: () => {
+
+            queryClient.invalidateQueries({
+
+                queryKey: ['teams']
+            })
+        }
+    })
+
+    return {
+        mutate,
+        isPending
+    }
+};
---
+++ FootballApp/src/queries/useGetTeamListQuery.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { useApi } from "../hooks/useApi";
+import { TeamType } from "../types";
+
+export const useGetTeamListQuery = () => {
+
+    const { apiGet } = useApi();
+
+    const { data, error, isFetching } = useQuery({
+
+        queryKey: ['teams'],
+        queryFn: async() => {
+
+            return apiGet<TeamType[]>('teams')
+        }
+    })
+
+    return {
+
+        data,
+        error,
+        isFetching
+    }
+}
