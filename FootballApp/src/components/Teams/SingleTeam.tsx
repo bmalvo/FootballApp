6c59361989cf170512707b051fb34681b7346208
bmalvo <boydmalvo@gmail.com>
2024-12-24 14:54:50 +0100

    create components to display teams

--- FootballApp/src/components/Teams/SingleTeam.tsx
+++ FootballApp/src/components/Teams/SingleTeam.tsx
@@ -1,30 +1,40 @@
-import { useDeleteTeam } from "../../hooks/useDeleteTeam";
-// import { useGetPlayersListQuery } from "./queries/useGetPlayersListQuery";
+import { useState } from "react";
 import { TeamType } from "../../types"
+import { DeleteTeamConfirmation } from "./DeleteTeamConfirmation";
 
 
 type SingleTeamProps = {
 
     team: TeamType;
-    onTeamRemove: (id: string) => void;
 }
 
 
-export const SingleTeam = ({ team, onTeamRemove }: SingleTeamProps) => {
-
-    // const { data: playersList } = useGetPlayersListQuery();
+export const SingleTeam = ({ team }: SingleTeamProps) => {
     
-    const { deleteTeam } = useDeleteTeam();
-
-    const handleDelete = () => {
-        
-        deleteTeam(team.id);
-        onTeamRemove(team.id);
+    const [mode, setMode] = useState<'none' | 'edit' | 'delete'>('none')
+    
+    const toggleEditMode = () => {
+    
+        setMode(prev => prev === "edit" ? 'none' : 'edit');
     }
 
+    const toggleDeleteMode = () => {
+
+        setMode(prev => prev === "delete" ? "none" : 'delete')
+    }
+
+
     return <>
-        <p>{team.Nazwa}</p>
-        <button onClick={handleDelete}>Usuń</button>
-        <button>Edytuj</button>
+        <li>
+            <p>{team.Nazwa}</p>
+            {mode === 'edit' ? 'edycja wkrótce...' : null}
+            <button onClick={toggleEditMode}>
+                {mode === 'edit' ? 'Cofnij' : 'Edytuj'}
+            </button>
+            {mode === 'delete' ? <DeleteTeamConfirmation team={team} /> : null}
+            <button onClick={toggleDeleteMode}>
+                {mode === 'delete' ? 'Cofnij' : 'Usuń'}
+            </button>
+        </li>
     </>
-}
+};
--- FootballApp/src/components/Teams/TeamsList.tsx
+++ FootballApp/src/components/Teams/TeamsList.tsx
@@ -1,45 +1,28 @@
-import { useQuery } from "@tanstack/react-query"
-import { useState } from "react";
-import { useTeamsList } from "../../hooks/useTeamsList";
-import { TeamForm } from "../../forms/TeamForm";
 import { SingleTeam } from "./SingleTeam";
 import { TeamType } from "../../types";
+import { useGetTeamListQuery } from "../../queries/useGetTeamListQuery";
 
 
 
 export const TeamsList = () => {
 
-    const [seeAdTeamForm, setSeeAdTeamForm] = useState(false);
-    const {removeTeam} = useTeamsList();
+    const { data: teams, error, isFetching } = useGetTeamListQuery();
 
-    const { data } = useQuery({
+    if (!teams) return <p>Brak drużyn w bazie</p>
+    if (isFetching) return <p>Wczytywanie danych...</p>
+    if (error) return <p>Wystąpił błąd: { error.message }</p>
 
-        queryKey: ['teams'],
-        queryFn: async () => {
-            
-            const response = await fetch('http://localhost:3000/teams');
-            return response.json();
-        }
-    })
-
-    if (!data) return <p>Brak drużyn w bazie</p>
-
-    const seeAdTeamFormHandle = () => {
-
-        setSeeAdTeamForm(prevState => !prevState);
-    }
-
+    
     return <>
         <ul>
             {
-                data.map((team: TeamType) =>
+                teams.map((team: TeamType) =>
                 <li key={team.id}>
-                    <SingleTeam team={team} onTeamRemove={removeTeam} />
+                    <SingleTeam team={team} />
                     </li>)
             }
         </ul>
-        {seeAdTeamForm? <TeamForm /> : null}
-        <button onClick={seeAdTeamFormHandle}>Dodaj drużynę</button>
+         {/* <AddTeam /> */}
 
      </>
 }
